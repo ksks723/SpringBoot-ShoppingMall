b5287ac3f204ebe3414f8761a3481a15e0811271
ksks723 <ksks723@gmail.com>
2023-06-18 23:52:09 +0900

    login

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -40,4 +40,15 @@ public class MemberController {
         }
         return "redirect:/";
     }
+
+    @GetMapping("/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping(value="/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg","아이디 또는 비번을 확인하세요.");
+        return "/member/memberLoginForm";
+    }
 }
